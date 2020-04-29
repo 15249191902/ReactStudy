719a8b20eafd6cba94b4ef9b6b8cd4b7e6ff274d
Rababa <546331130@qq.com>
2020-04-29 23:57:54 +0800

    4-28

--- src/component/Children.js
+++ src/component/Children.js
@@ -27,24 +27,6 @@ class Children extends React.Component {
         list: list
       })
     })
-    // axios({
-    //   method: 'get',
-    //   url: '/users/getUser',
-    // }).then(res => {
-    //   console.log(res);
-    // })
-    // fetch("https://api.example.com/items")
-    // .then(res => res.json())
-    // .then(
-    //   (result) => {
-    //     console.log(result)
-    //   },
-    //   // 注意：需要在此处处理错误
-    //   // 而不是使用 catch() 去捕获错误
-    //   // 因为使用 catch 去捕获异常会掩盖掉组件本身可能产生的 bug
-    //   (error) => {
-    //   }
-    // )
   }
   render () {
     return (
