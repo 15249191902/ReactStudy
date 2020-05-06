f4c81284d1c73848f4068f02118ea286be380287
3172236129 <3172236129@qq.com>
2020-05-06 17:21:31 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,22 @@ import {
     Switch,
     Route,
 } from 'react-router-dom'
-import Home from "../pages/home/Home.js"
-import Message from "../pages/Message.js"
-import Detail from "../pages/Detail.js"
+// import Home from "../pages/home/Home.js"
+// import Message from "../pages/Message.js"
+// import Detail from "../pages/Detail.js"
+import {mainRouter} from "./routerConfig.js"
 export default function Router() {
     return (
         <div>
             <BrowserRouter>
-                {/* <NavLink to="Message" activeClassName="hurray">Message</NavLink>
-                <NavLink to="Detail" activeClassName="hurray">Detail</NavLink> */}
                 <Switch>
-                    <Route path="/Message">
-                        <Message />
-                    </Route>
-                    <Route path="/Detail">
-                        <Detail />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
+                    {
+                      mainRouter.map(item => {
+                        return <Route key={item.path} component={item.component} path={item.path} exact={item.exact}></Route>
+                      })
+                    }
                 </Switch>
+                
             </BrowserRouter>
         </div>
     )
---
+++ src/router/routerConfig.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from "../pages/home/Home";
+import Message from "../pages/Message.js"
+import Detail from "../pages/Detail.js"
+export const mainRouter = [
+  {
+    component: Home,
+    path: '/',
+    exact: true,
+  },
+  {
+    component: Detail,
+    path: '/Detail'
+  },
+  {
+    component: Message,
+    path: '/Message'
+  },
+]
